9def2ed2a2e9c9b7a8bd2fb12ccaa55331d85a19
Juli Tay <julitays@icloud.com>
2023-01-11 13:31:21 +0300

    +bg

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from tkinter import *
 root = Tk()
 root.geometry('400x200')
 root.title('Игра в кости. Сделай бросок.')
-
-
-
+root.resizable(height = False, width= False)
+root.iconphoto(True, PhotoImage(file=('/home/julitay/-game_of_dice/icons/icon.png')))
+font = PhotoImage(file=('/home/julitay/-game_of_dice/icons/pngegg.png'))
+Label(root, image=font).pack()
 
 root.mainloop()
 
